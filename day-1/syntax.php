13258998257fbd53a08e31b31053ca7ff6a08b79
mdalmin291 <alamin291mohammad@gmail.com>
2020-10-04 20:36:55 +0600

    new

---
+++ day-1/syntax.php
@@ -0,0 +1,181 @@
+
+
+<!--<html>
+<head>
+   <title><?php /*echo 'hello world';*/?></title>
+</head>
+<body>
+    <h1><?php /*echo 'This is a heading';*/?></h1>
+    <p><?php /*echo 'This is a paragaraph';*/?></p>
+
+
+     <input type="text" value="<?php /*echo 'hello world';*/?>">
+
+</body>
+</html>-->
+<?php
+
+$x = 20;
+do {
+    echo $x.'<br/>';
+    $x++;
+
+}while ($x<100)
+
+
+
+
+/*$i = 10;
+while ($i<20){
+    echo $i.'<br/>';
+    $i++;
+}*/
+/*for ($x = 200; $x<=700; $x+=4){
+    echo $x.'<br/>';
+
+}*/
+/*for($x =1; $x<=5; $x++ ){
+    echo 'hello world <br/>';
+}*/
+
+/*$firstname = 'rahim';
+$lastname = 'karim';
+echo "full name is" .$firstname.' '.$lastname;*/
+  
+//$name = 'rahim';
+//echo $name;
+/*
+     * Arithmatic operator
+          * Binary(+,-,*,/)
+          * unary (++,--,-)
+     * Assignment operator
+          * (+=,-+,*=, /+,.=)
+     * conditional operator
+           * *>, >=,<=, ==, !=,===,!==
+     *Logical operator
+        *   &&, ||, !
+ */
+
+/*
+ * single line statement
+ * conditional statement
+      * if
+      *if else
+      *if else if
+      *switch
+ * repeated statement
+      * while
+      *Do while
+      * for
+      * for each
+ */
+
+
+//single line statement
+/*$x = 30;
+$y = 20;
+$z = $x+$y;
+echo $z;*/
+
+//conditional statement
+/*$x = 30;
+$y = 20;
+$z = 40;
+
+
+if ($x<$y) {
+    echo 'condition false';
+} else if($z<$y){
+    echo 'condition false';
+}else if($x>$z){
+    echo 'condition true';
+} else{
+    echo 'it going fine';
+}*/
+
+//switch
+
+/*$x = 10;
+$y = 20;
+$z = 30;
+
+switch ($x){
+    case 10:
+        echo 'Hello world';
+        break;
+    case 20:
+        echo 'bangladesh';
+        break;
+    case 30:
+        echo 'nobody';
+        break;
+
+    default :
+        echo 'oke its fine';
+}*/
+
+
+
+
+
+/*if ($x<$y) {
+    $z = $x+$y;
+    echo $z;
+}else{
+    echo 'Hello world';*/
+
+
+
+
+//$a = true;
+//echo $a;
+
+//echo ($x<$y) || ($y == $x);
+
+/*echo ($x>$y)&&($y<$z);
+
+echo ($x==$y) && ($y<$z);*/
+
+
+
+
+
+/*echo $x+=$y;
+echo '<br/>';
+echo $x-=$y;
+echo '<br/>';
+echo $x*=$y;
+echo '<br/>';
+echo $x/$y;
+echo '<br/>';
+echo $x%$y;
+echo '<br/>';
+echo $x.=$y;*/
+
+
+
+
+
+/*echo $x++;
+echo '<br/>';
+echo $x;
+
+echo $x--;
+echo '<br/>';
+echo $x;*/
+
+
+//arithmetic operator
+/*echo $X+$Y;
+echo '<br/>';
+echo $X-$Y;
+echo '<br/>';
+echo $X*$Y;
+echo '<br/>';
+echo $X/$Y;
+echo '<br/>';
+echo $X%$Y;*/
+
+
+
+?>
